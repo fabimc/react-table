9002f890bd9c52ad3a4ab57853220dc9a1e9832d
Fabián Montealegre <fabimc@gmail.com>
2024-07-10 16:13:16 -0600

    adds better tests

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,19 +1,26 @@
-import {expect, test} from 'vitest'
+import { expect, test } from 'vitest'
 import AppWithProviders from './App'
-import { act, fireEvent, render, screen } from '@testing-library/react'
+import { act, fireEvent, render, screen, within } from '@testing-library/react'
 
 test('renders App', () => {
-  render(<AppWithProviders />);
+  render(<AppWithProviders />)
 })
 
 test('edit mode works', async () => {
-  render(<AppWithProviders />);
-  const editButtons = await screen.findAllByTestId('EditIcon');
-  
+  render(<AppWithProviders />)
+  const editButtons = await screen.findAllByTestId('EditIcon')
+
   act(async () => {
-    fireEvent.click(editButtons[0]);
-    const saveButtons = await screen.findAllByTestId('SaveIcon');
-    expect(saveButtons[0]).toBeDefined()
-  });
-  
+    fireEvent.click(editButtons[0])
+    const saveButtons = await screen.findAllByTestId('SaveIcon')
+    const saveButton = saveButtons[0]
+    expect(saveButton).toBeDefined()
+
+    const firstName: HTMLInputElement = screen.getByPlaceholderText('First Name')
+    expect(firstName).toBeDefined()
+    firstName.value = ''
+    fireEvent.click(saveButton)
+    const errorMessage = screen.findByText('First Name is Required')
+    expect(errorMessage).toBeDefined()
+  })
 })
